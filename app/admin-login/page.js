aacbef29ad2071b8967cc260c03cf8774b17f315
Ahmet Yıldız <78380442+yildizahmett@users.noreply.github.com>
2024-01-04 17:06:00 +0300

    Update page.js

--- app/admin-login/page.js
+++ app/admin-login/page.js
@@ -17,7 +17,7 @@ export default function Page() {
 			return;
 		}
 
-		await fetch(`${process.env.NEXT_PUBLIC_API_URL}/admin/login`, {
+		await fetch(`${process.env.NEXT_PUBLIC_API_URL_ADMIN}/admin/login`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
